660cd598c3fdd4341093288eaea3d545f72b97bd
Abram Symons <abramsymons@gmail.com>
2020-09-07 14:59:16 +0430

    add telegram support

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ SEALING_BORDER = 5
 SENT_TIMEOUT = 60 * 60
 CHECK_INTERVAL = 15
 
-KEYBASE_BOT_KEY = 'PUT_BOT_PAPER_KEY_HERE'
+KEYBASE_BOT_KEY = 'PUT_YOUR_KEY_HERE'
 KEYBASE_BOT_USERNAME = 'idchain_bot'
 KEYBASE_BOT_CHANNEL = {'name': 'idchain', 'topic_name': 'alerts','members_type': 'team'}
+
+TELEGRAM_BOT_KEY = 'PUT_YOUR_KEY_HERE'
+TELEGRAM_BOT_CHANNEL = '@idchain_alerts'
--- main.py
+++ main.py
@@ -7,14 +7,25 @@ import pykeybasebot.types.chat1 as chat1
 from config import *
 
 sent = {}
-bot = Bot(username=KEYBASE_BOT_USERNAME, paperkey=KEYBASE_BOT_KEY, handler=None)
+keybaseBot = None
+if KEYBASE_BOT_KEY:
+    keybaseBot = Bot(username=KEYBASE_BOT_USERNAME, paperkey=KEYBASE_BOT_KEY, handler=None)
+
 def alert(msg):
     if msg in sent and time.time() - sent[msg] < SENT_TIMEOUT:
         return
     print(time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime()), msg)
     sent[msg] = int(time.time())
-    channel = chat1.ChatChannel(**KEYBASE_BOT_CHANNEL)
-    asyncio.run(bot.chat.send(channel, msg))
+    if KEYBASE_BOT_KEY:
+        channel = chat1.ChatChannel(**KEYBASE_BOT_CHANNEL)
+        asyncio.run(keybaseBot.chat.send(channel, msg))
+    if TELEGRAM_BOT_KEY:
+        payload = json.dumps({"chat_id": TELEGRAM_BOT_CHANNEL, "text": msg})
+        headers = {'content-type': "application/json", 'cache-control': "no-cache"}
+        url = f'https://api.telegram.org/bot{TELEGRAM_BOT_KEY}/sendMessage'
+        r = requests.post(url, data=payload, headers=headers)
+        print(r.text)
+
 
 def check():
     payload = json.dumps({"jsonrpc": "2.0", "method": "clique_status", "params": [], "id": 1})
